88f1864479f1655c4fd1d00b29301e5b061d219d
2968629681 <2968629681@qq.com>
2023-08-22 10:15:07 +0800

    依托答辩

---
+++ inject.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codegangsta/inject"
+)
+
+type S1 interface{}
+type S2 interface{}
+
+type Staff struct {
+	Name    string `inject`
+	Company S1     `inject`
+	Level   S2     `inject`
+	Age     int    `inject`
+}
+
+func main() {
+	s := Staff{}
+
+	inj := inject.New()
+
+	inj.Map("tom")
+	inj.MapTo("Tencent", (*S1)(nil))
+	inj.MapTo("T4", (*S2)(nil))
+	inj.Map(23)
+	inj.Map("toasdasdm")
+
+	inj.Apply(&s)
+
+	fmt.Printf("s=%v\n", s)
+}
